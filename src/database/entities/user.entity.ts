e77a6076e7c70466dc1f4a843fb1cff9748fced4
Almor21 <eanoriega@uninorte.edu.co>
2025-07-05 18:42:40 -0500

    fix: update user entity to change id type

--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -3,7 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateCol
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column()
   firstName: string;
