396d9153d553de0f794fa615d85bfa5742fcd0b2
Almor21 <eanoriega@uninorte.edu.co>
2025-07-05 15:06:43 -0500

    feat: add auth login and sign up dtos

---
+++ src/auth/dtos/signup.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
+}
