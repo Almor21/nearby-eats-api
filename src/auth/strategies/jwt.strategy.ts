42c787cb0e7dba0ac87395ba663dae26d66971ed
Almor21 <eanoriega@uninorte.edu.co>
2025-07-06 00:25:28 -0500

    feat: add jwt blacklist validation

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,12 +3,15 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { AuthService } from '../auth.service';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { TokenService } from '../token/token.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     configService: ConfigService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly tokenService: TokenService,
   ) {
     super({
       secretOrKey: configService.get('JWT_SECRET', ''),
@@ -17,7 +20,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: any) {
+  async validate(payload: JwtPayload) {
+    await this.tokenService.verifyTokenBlackList(payload.jti);
+
     return this.authService.validateUser(payload.id);
   }
 }
