d3f17fe497cc05ac3e7008fe830299dc720e4dd2
Almor21 <eanoriega@uninorte.edu.co>
2025-07-06 09:24:01 -0500

    feat: add Docker support with Dockerfile and docker-compose configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,16 +9,26 @@ import { ConfigService } from '@nestjs/config';
     TypeOrmModule.forFeature(entities),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST', ''),
-        port: parseInt(config.get('DB_PORT', '')),
-        username: config.get('DB_USERNAME', ''),
-        password: config.get('DB_PASSWORD', ''),
-        database: config.get('DB_NAME', ''),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false,
-      }),
+      useFactory: (config: ConfigService) => {
+        if (config.get('NODE_ENV') === 'docker') {
+          return {
+            type: 'postgres',
+            url: config.get('DATABASE_URL', ''),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: false,
+          };
+        }
+        return {
+          type: 'postgres',
+          host: config.get('DB_HOST', ''),
+          port: parseInt(config.get('DB_PORT', '')),
+          username: config.get('DB_USERNAME', ''),
+          password: config.get('DB_PASSWORD', ''),
+          database: config.get('DB_NAME', ''),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: false,
+        };
+      },
     }),
   ],
   exports: [TypeOrmModule],
--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -4,14 +4,28 @@ import { configDotenv } from 'dotenv';
 
 configDotenv();
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_NAME || 'db',
-  entities: entities,
-  migrations: ['src/database/migrations/*.ts'],
-  synchronize: false,
-});
+let AppDataSource: DataSource;
+
+if (process.env.NODE_ENV === 'docker') {
+  AppDataSource = new DataSource({
+    type: 'postgres',
+    url: process.env.DATABASE_URL || '',
+    entities: entities,
+    migrations: ['src/database/migrations/*.ts'],
+    synchronize: false,
+  });
+} else {
+  AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'db',
+    entities: entities,
+    migrations: ['src/database/migrations/*.ts'],
+    synchronize: false,
+  });
+}
+
+export default AppDataSource;
