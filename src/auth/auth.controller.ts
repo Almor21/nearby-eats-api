eeae09bbe5849d11e735e98c891020e9f86cf238
Almor21 <eanoriega@uninorte.edu.co>
2025-07-06 02:18:21 -0500

    refactor: add skip log fields to login route to hide sensitive information

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,17 @@ import { LoginDto } from './dtos/login.dto';
 import { SignUpDto } from './dtos/signup.dto';
 import { Public } from 'src/decorators/public.decorator';
 import { Token } from 'src/decorators/token.decorator';
+import { SkipLogFields } from 'src/decorators/skip-log-fields.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
+  @SkipLogFields({
+    request: ['password'],
+    response: ['token'],
+  })
   @Post('login')
   async login(@Body() data: LoginDto) {
     return await this.authService.login(data);
